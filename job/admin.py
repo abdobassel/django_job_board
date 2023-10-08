0fc92660f3ac00be690d96a3fb5c5311bdb0479d
abdobassel <abdobassel@gmail.com>
2023-10-08 23:40:14 +0300

    add job model and listdisplay and filter

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Job)
+class jobAdmin(admin.ModelAdmin):
+    list_display = ['title','job_type','published_at','salary','experience']
+    list_filter = ["salary", "experience"]
--- job/models.py
+++ job/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    job_choices = [('Fulltime','Fulltime'),
+                    ('Parttime','Parttime'),
+    ]
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=50,choices=job_choices,default='')
+    description = models.TextField(max_length=3000,null=True,blank=True)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    #category
+    experience = models.IntegerField(default=1)
+    #apply job 
+    #post job
+
+
+
+
+
+    def __str__(self):
+        return str(self.title)
