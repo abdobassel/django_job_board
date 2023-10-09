357b1969735de62b1b551b6ae75bf05165a670dc
abdobassel <abdobassel@gmail.com>
2023-10-09 03:50:31 +0300

    added paginator and my touch with paginator and edit urls

--- job/views.py
+++ job/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render,Http404,HttpResponse,get_object_or_404
 from .models import *
+from django.core.paginator import Paginator,Page,PageNotAnInteger
 
 def job_list(request):
     job_list =Job.objects.all()
-    context = {'jobs':job_list}
+    
+
+    paginator = Paginator(job_list, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    context = {'jobs':page_obj}
 
 
     return render(request, 'job/job_list.html',context)
